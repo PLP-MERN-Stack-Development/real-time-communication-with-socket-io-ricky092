36858792c34fc0ff35e8b82dc22eeb85f49010fb
ricky092 <Lamiodun8@gmail.com>
2025-12-04 03:20:19 +0100

    Configure for GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/real-time-communication-with-socket-io-ricky092/',
   server: {
     port: 5173,
   },
